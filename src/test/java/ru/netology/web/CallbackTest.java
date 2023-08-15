55705d6685a1c325991fde0ea62d8caa8a9c58fb
Oleg <olegkajlev@gmail.com>
2023-08-15 12:43:14 +0300

    test commit

---
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -0,0 +1,24 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+public class CallbackTest {
+
+    @Test
+    void shouldTest() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $(".form_theme_alfa-on-white");
+
+        form.$("[data-test-id=name] input").setValue("Оксана");
+        form.$("[data-test-id=phone] input").setValue("+79880000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$(".button_theme_alfa-on-white").click();
+       // $(".alert-success").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $(".paragraph_theme_alfa-on-white").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        Thread.sleep(5000);
+    }
+}
